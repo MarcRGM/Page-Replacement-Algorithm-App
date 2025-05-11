5f910987b3cb1943a1ae8152d4491d81e656a5a5
MarcRGM <marcrodniemanalo@gmail.com>
2025-05-11 14:28:59 +0800

    Functionalities and Page-reference string animation

--- src/main/java/com/pagereplacementalgorithm/algorithms/PageReplacement.java
+++ src/main/java/com/pagereplacementalgorithm/algorithms/PageReplacement.java
@@ -1,4 +1,28 @@
 package com.pagereplacementalgorithm.algorithms;
 
 public class PageReplacement {
+
+    public enum algoCategories {
+        FIFO("FIRST IN FIRST OUT (FIFO)", "FIFO is the most basic page replacement method. It uses a queue to monitor the order of pages loaded into memory, where the oldest page stays at the front. When replacement is needed, the page that entered first (at the front) gets removed."),
+        OPTIMAL("OPTIMAL", "The Optimal algorithm replaces the page that won’t be used for the longest time in the future. It looks ahead in the reference string and chooses the page that’s not needed anytime soon, which minimizes page faults."),
+        LRU("LEAST RECENTLY USED (LRU)", "LRU replaces the page that hasn’t been used for the longest time. It assumes that pages used recently will likely be needed again soon, so it removes the one that was accessed the farthest back in time.");
+
+        private final String displayName;
+        private final String info;
+
+        algoCategories(String displayName, String info) {
+            this.displayName = displayName;
+            this.info = info;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+    }
+
+
 }
--- src/main/java/com/pagereplacementalgorithm/controller/AppController.java
+++ src/main/java/com/pagereplacementalgorithm/controller/AppController.java
@@ -1,4 +1,112 @@
 package com.pagereplacementalgorithm.controller;
 
-public class AppController {
+import com.pagereplacementalgorithm.algorithms.PageReplacement;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+
+    Random random = new Random();
+    int[] referenceString = new int[20];
+    Timeline timeline = new Timeline();
+    Double delay = 0.3;
+
+    @FXML
+    private ComboBox<String> algoCombo;
+
+    @FXML
+    private Slider frameSlider;
+
+    @FXML
+    private Label frameNum, algoInfo, algoLabel;
+
+    @FXML
+    private Button generateButton, startButton, restartButton;
+
+    @FXML
+    VBox infoVBox;
+
+    @FXML
+    HBox refBox;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setCategories();
+
+        // Update the label in real time as the slider changes
+        frameSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            frameNum.setText(String.valueOf(newValue.intValue()));
+        });
+
+    }
+
+    private void setCategories() {
+        algoCombo.getItems().setAll(Arrays.stream(PageReplacement.algoCategories.values())
+                .map(PageReplacement.algoCategories::getDisplayName)
+                .toArray(String[]::new));
+    }
+
+    @FXML
+    private void generateClicked() {
+        for (int i = 0; i < 20; i++) {
+            referenceString[i] = random.nextInt(10); // 0-9
+        }
+
+        refBox.getChildren().clear(); // clear old boxes
+        for (int num : referenceString) {
+            Label box = new Label(String.valueOf(num));
+            box.getStyleClass().add("number-box");
+            refBox.getChildren().add(box);
+        }
+
+    }
+
+    @FXML
+    private void startClicked() {
+        String selectedName = algoCombo.getValue();
+
+        PageReplacement.algoCategories selectedEnum = Arrays.stream(PageReplacement.algoCategories.values())
+            .filter(e -> e.getDisplayName().equals(selectedName))
+            .findFirst()
+            .orElse(null);
+
+        if (selectedEnum != null) {
+            infoVBox.setVisible(true);
+            algoLabel.setText(selectedEnum.getDisplayName());
+            algoInfo.setText(selectedEnum.getInfo());
+        }
+
+        for (int i = 0; i < refBox.getChildren().size(); i++) {
+            int index = i;
+
+            KeyFrame keyFrame = new KeyFrame(Duration.seconds(i * delay), e -> {
+                Label label = (Label) refBox.getChildren().get(index);
+                label.getStyleClass().add("green-box");
+            });
+
+            timeline.getKeyFrames().add(keyFrame);
+        }
+
+        timeline.play();
+
+    }
+
+    @FXML
+    private void restartClicked() {}
+
 }
